8236842eaceac5be856f2421a1c7ac5dbe4db3b3
AmirtterK <strikereureka7661@gmail.com>
2025-07-15 02:56:48 +0100

    galleria fix

---
+++ src/nav-bar.js
@@ -0,0 +1,14 @@
+let lastScrollY = window.scrollY;
+const navBar = document.querySelector(".nav-bar");
+
+window.addEventListener("scroll", () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > lastScrollY) {
+    navBar.classList.add("nav-hidden");
+  } else {
+    navBar.classList.remove("nav-hidden");
+  }
+
+  lastScrollY = currentScrollY;
+});
